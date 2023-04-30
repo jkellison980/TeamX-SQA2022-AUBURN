65a251a0e1dc496c2123fac47a2c443761156e94
Matthew <matthew.mark.pichler@gmail.com>
2023-04-30 16:18:16 -0500

    fixed logging

--- KubeSec-master/logger.py
+++ KubeSec-master/logger.py
@@ -1,7 +1,7 @@
 import logging
 
 def initLogObj():
-    format_str = '%(ascTime)s:%(message)s'
+    format_str = '%(asctime)s %(message)s'
     file_name = 'FORENSIC_LOG.log'
     logging.basicConfig(format=format_str, filename=file_name, level=logging.INFO)
     loggerObj = logging.getLogger('logger')
